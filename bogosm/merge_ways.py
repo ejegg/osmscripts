0a5b8bc2576fd004cfa13400be8e4ecd4eaee0bb
Elliott Eggleston <ejegg@ejegg.com>
2017-09-23 19:39:16 -0500

    Write building parts, don't duplicate nodes

--- bogosm/merge_ways.py
+++ bogosm/merge_ways.py
@@ -3,7 +3,7 @@ from __future__ import print_function
 import osmium
 import shapely.wkb
 import shapely.ops
-from pprint import pprint
+import shapely.geometry
 import sys
 from osmwriter import OSMWriter
 
@@ -31,39 +31,84 @@ class BogotaReader(osmium.SimpleHandler):
 
 
 class BogotaMerger:
+    node_index = -1
+    nodes = {}
+    way_index = -1
+
     def merge_building_parts(self, input_file, output_file):
         wm = BogotaReader()
         wm.apply_file(input_file)
         codigos = wm.buildingParts.keys()
-        nodeidx = -1
         with open(output_file, "w") as fp:
             xml = OSMWriter(fp=fp)
             for k in codigos:
-                shapely_shapes = list(map(
-                    lambda part: shapely.wkb.loads(part['shape'], hex=True),
-                    wm.buildingParts[k]
-                ))
+                shapely_shapes = []
+                for part in wm.buildingParts[k]:
+                    shapely_shape = shapely.wkb.loads(part['shape'], hex=True)
+                    shapely_shapes.append(shapely_shape)
+                    self.write_shape(
+                        xml,
+                        shapely_shape,
+                        {
+                            'building:part': 'yes',
+                            'building:levels': part['levels'],
+                            'ref:BOG:ConCodigo': k
+                        }
+                    )
+                # write the full building out
                 max_levels = max(map(
                     lambda part: part['levels'],
                     wm.buildingParts[k]
                 ))
                 summed = shapely.ops.cascaded_union(shapely_shapes)
-                nodeids = []
-                for point in summed.exterior.coords:
-                    xml.node(nodeidx, point[1], point[0])
-                    nodeids.append(nodeidx)
-                    nodeidx -= 1
-                # write the full building out
-                xml.way(
-                    -1,
+                self.write_shape(
+                    xml,
+                    summed,
                     {
                         'building': 'yes',
-                        'levels': max_levels,
+                        'building:levels': max_levels,
                         'ref:BOG:ConCodigo': k
-                    },
-                    nodeids
+                    }
                 )
+
             xml.close()
+
+    def write_shape(self, xml, shape, tags):
+        """ Write a shapely shape as an OSM way
+        :type xml: OSMWriter
+        :type shape: shapely.geometry.BaseGeometry
+        :type tags: dict
+        """
+        node_ids = []
+
+        if hasattr(shape, 'exterior'):
+            coords = shape.exterior.coords
+        elif hasattr(shape, 'boundary') and hasattr(shape.boundary, 'geoms'):
+            first_exterior = shape.boundary.geoms[0]
+            coords = first_exterior.coords
+        else:
+            raise RuntimeError(
+                "Can't deal with shape type {0}".format(
+                    type(shape)
+                ))
+
+        for point in coords:
+            node_id = self.write_point(xml, point)
+            node_ids.append(node_id)
+        xml.way(self.way_index, tags, node_ids)
+        self.way_index -= 1
+
+    def write_point(self, xml, point):
+        """ Write a point as an OSM node, avoiding duplication
+        :type xml: OSMWriter
+        :type point: float[]
+        """
+        key = "{0}|{1}".format(point[1], point[0])
+        if key not in self.nodes:
+            xml.node(self.node_index, point[1], point[0])
+            self.nodes[key] = self.node_index
+            self.node_index -= 1
+        return self.nodes[key]
 
 
 BogotaMerger().merge_building_parts(sys.argv[1], sys.argv[2])
